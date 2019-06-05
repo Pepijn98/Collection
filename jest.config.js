d6a4bf4d881646b87b4c1ae62ddf54682f8058b1
KurozeroPB <root@kurozeropb.info>
2019-06-05 12:52:56 +0200

    Add quick test, really needs an update

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "collection",
+    verbose: true,
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["<rootDir>/test/*.test.ts"]
+};
---
+++ test/collection.test.ts
@@ -0,0 +1,28 @@
+import Collection from "../";
+
+describe("Collection", () => {
+    describe("#from()", () => {
+        const arr = ["foo", "bar", "baz", "test"];
+        const obj = { foo: "foo", bar: "bar", baz: "baz", test: "test" };
+
+        const testColl1 = new Collection<string>(String);
+        testColl1.add("foo");
+        testColl1.add("bar");
+        testColl1.add("baz");
+        testColl1.add("test");
+
+        const testColl2 = new Collection<string>(String);
+        testColl2.set("foo", "foo");
+        testColl2.set("bar", "bar");
+        testColl2.set("baz", "baz");
+        testColl2.set("test", "test");
+
+        it("should be equal to collections made manually", () => {
+            const aColl = Collection.from(arr);
+            const oColl = Collection.from(obj);
+
+            expect(aColl).toStrictEqual(testColl1);
+            expect(oColl).toStrictEqual(testColl2);
+        });
+    });
+});
